9c297fb8e40115614cba7a622fe9522035538ae2
Anand <anandchakru@gmail.com>
2022-02-27 22:30:28 -0800

    Add Images button

--- src/features/album/Album.tsx
+++ src/features/album/Album.tsx
@@ -6,11 +6,20 @@ import { Box, Card, CardActionArea, CardContent, CardMedia, Grid, SxProps, Typog
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import Dialog from '@mui/material/Dialog'
-
+import AddAPhotoIcon from './add_photo_alternate_black_24dp.svg'
+const opaqueOnBlur = {
+  opacity: 0.5,
+  transitionProperty: 'opacity',
+  transitionDelay: '0.1s',
+  '&:hover': {
+    opacity: 1,
+    backgroundColor: '#33333344',
+  }
+} as SxProps
 const fsButtonStyles = {
+  ...opaqueOnBlur,
   position: 'absolute',
   top: 0,
-  opacity: 0.5,
   width: '35vw',
   height: '100vh',
   display: 'flex',
@@ -71,7 +80,7 @@ function Album() {
                 setCurrentImage(index)
               }} />
             <CardContent>
-              <Typography variant="body2" color="text.secondary">
+              <Typography variant="body2">
                 {image.name}
               </Typography>
               <Typography variant="caption" display="block" color="text.secondary">
@@ -79,7 +88,26 @@ function Album() {
               </Typography>
             </CardContent>
           </CardActionArea>
-        </Card></Grid>)}
+        </Card>
+        </Grid>)}
+        <Grid key="add_an_img" item xs={6} sm={4} md={3} lg={2}>
+          <Card sx={{ maxWidth: 250, }}>
+            <CardActionArea>
+              <CardMedia component="img" height="250" src={AddAPhotoIcon} alt="Add an image" sx={{
+                ...opaqueOnBlur,
+              }}
+                onClick={() => { alert('WIP') }} />
+              <CardContent>
+                <Typography variant="body2">
+                  Pick images
+                </Typography>
+                <Typography variant="caption" display="block" color="text.secondary">
+                  compresed before upload
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
       </Grid>
       <FullscreenImage base={albumGhPageImages?.repoInfo.data.homepage}
         currentImage={albumGhPageImages?.img[currentImage]}
