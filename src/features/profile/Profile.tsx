fde0f8c5256dc0a10cd5cc047d9462a55b37f6aa
Anand <anandchakru@gmail.com>
2023-04-01 01:21:54 -0700

    display email on profile page

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,9 +1,15 @@
 import { Typography } from "@mui/material"
+import { useAppSelector } from "../../app/hooks"
+import { AppUser, selectAuthUser } from "../auth/AuthSlice"
 
 const Profile = () => {
+  const authUser = useAppSelector(selectAuthUser)
   return <>
     <Typography variant="h3" component="div">
       Profile
+    </Typography>
+    <Typography variant="subtitle2" component="div">
+      {authUser?.email}
     </Typography>
   </>
 }
